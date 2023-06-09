42d1c0cc4e2cac6d8df7271937f19fa3ae2ad01e
vab1997 <victorbejas97@gmail.com>
2023-06-09 13:41:27 -0300

    create api send form

---
+++ src/app/api/send-form-utility/route.ts
@@ -0,0 +1,24 @@
+import { kv } from '@vercel/kv'
+
+export async function POST(request: Request) {
+  const { title, description, use_case, code, language } = await request.json()
+
+  console.log({ title, description, use_case, code, language })
+
+  if (!title || !description || !use_case || !code || !language) {
+    return new Response('Missing required fields', { status: 400 })
+  }
+
+  const id = crypto.randomUUID()
+  const timestamp = Date.now()
+
+  try {
+    await kv.set(`utlity-${id}`, { title, description, use_case, code, language, timestamp })
+
+    return new Response('Contact saved!', { status: 200 })
+  } catch (error) {
+    console.error(error)
+    return new Response('Internal error', { status: 500 })
+  }
+
+}
