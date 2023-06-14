cd534c9e7d318da7236ab384d7c0ccd12d31a2e8
vab1997 <victorbejas97@gmail.com>
2023-06-14 15:22:20 -0300

    create constants

---
+++ src/utils/constant.ts
@@ -0,0 +1,26 @@
+export const codeExample = `import { Dispatch, SetStateAction, useState } from 'react'
+
+interface UseCounterOutput {
+  count: number
+  increment: () => void
+  decrement: () => void
+  reset: () => void
+  setCount: Dispatch<SetStateAction<number>>
+}
+
+export function useCounter(initialValue?: number): UseCounterOutput {
+  const [count, setCount] = useState(initialValue || 0)
+
+  const increment = () => setCount(x => x + 1)
+  const decrement = () => setCount(x => x - 1)
+  const reset = () => setCount(initialValue || 0)
+
+  return {
+    count,
+    increment,
+    decrement,
+    reset,
+    setCount,
+  }
+}
+`
