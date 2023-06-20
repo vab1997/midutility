343d2192d1e3e0e5558b05f9a63e10f713fcf51a
vab1997 <victorbejas97@gmail.com>
2023-06-20 19:28:13 -0300

    delete form old

--- src/app/create-utility/page.tsx
+++ src/app/create-utility/page.tsx
@@ -1,11 +1,9 @@
-// import { Form } from '@/components/Form'
 import { FormRedesing } from '@/components/FormRedesing'
 
 export default function CreateUtilityPage () {
   return (
     <main className='flex flex-col items-center justify-between h-full w-full'>
       <div className='flex flex-col items-center justify-center px-10 overflow-hidden h-full w-full pt-12'>
-        {/* <Form /> */}
         <FormRedesing />
       </div>
     </main>
--- src/app/list-utilities/page.tsx
+++ src/app/list-utilities/page.tsx
@@ -1,7 +1,7 @@
-export default function ListUtillitiePage() {
+export default function ListUtillitiePage () {
   return (
     <div>
       <h1>Welcome to ListUtillitiePage!</h1>
     </div>
-  );
+  )
 }
