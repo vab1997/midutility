6f942e6a999fe62a0c0b74a229404acbec7f8156
vab1997 <victorbejas97@gmail.com>
2023-06-21 01:48:10 -0300

    add string type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,6 @@ export function nonNullable<T> (value: T | null): value is NonNullable<T> {
   return value != null
 }
 
-export function formatDate (timestamp: number) {
+export function formatDate (timestamp: number | string) {
   return Intl.DateTimeFormat('en', { month: 'long', day: 'numeric', year: 'numeric' }).format(new Date(timestamp))
 }
