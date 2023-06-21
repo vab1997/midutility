9ca5c803cd0331391db612a53d6ac3b7ff8b1cbd
vab1997 <victorbejas97@gmail.com>
2023-06-21 00:17:45 -0300

    update property

--- src/components/ListOfUtilities.tsx
+++ src/components/ListOfUtilities.tsx
@@ -1,20 +1,10 @@
 import { useGetRangeUtilities } from '@/hooks/useGetRangeUtilities'
-import { Utility } from './Utility'
-import { getUtilityHtmlCssJs } from '@/services/clientService'
+import { Utility } from '@/components/Utility'
 
 export async function ListOfUtilities () {
   const listUtilities = await useGetRangeUtilities({ limit: 10 })
 
-  const listSup = await getUtilityHtmlCssJs()
-  console.log(listSup)
-
-  if (typeof listUtilities !== 'object') {
-    return (
-      <div className='flex items-center justify-center w-full h-full'>
-        <h1 className='text-6xl text-[#EE81C3] font-bold'>{listUtilities}</h1>
-      </div>
-    )
-  }
+  if (listUtilities === null) return null
 
   return (
     <section className='flex flex-col gap-12 px-10 py-16 w-full'>
@@ -22,10 +12,10 @@ export async function ListOfUtilities () {
       <div className='flex items-center justify-center gap-4'>
         {listUtilities.map((utility) => (
           <Utility
-            key={utility.key}
-            id={utility.key}
-            title={utility.title}
-            description={utility.description}
+            key={utility.id}
+            id={utility.id}
+            title={utility.title ?? ''}
+            description={utility.description ?? ''}
           />
         ))}
       </div>
