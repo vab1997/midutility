a8f5468d301b3eea2ebec7bf96d1146f75fa5c6c
vab1997 <victorbejas97@gmail.com>
2023-06-27 21:44:15 -0300

    change directory

--- src/app/list-utilities/detail/[idUtility]/page.tsx
+++ src/app/list-utilities/detail/[idUtility]/page.tsx
@@ -3,6 +3,10 @@ import { ButtonCopyClipboard } from '@/components/ButtonCopyClipboard'
 
 import { useGetUtility } from '@/hooks/useGetUtility'
 import { formatDate } from '@/utils'
+import { HtmlIcon } from '@/components/icons/HtmlIcon'
+import { CssIcon } from '@/components/icons/CssIcon'
+import { JsIcon } from '@/components/icons/JsIcon'
+import { TsIcon } from '@/components/icons/TsIcon'
 
 export default async function DetailUtility ({ params }: { params: { idUtility: string } }) {
   const { idUtility } = params
@@ -19,12 +23,12 @@ export default async function DetailUtility ({ params }: { params: { idUtility: 
   const { title, description, useCase, timestamp, codeHtml, codeCss, codeJs, codeTs } = utility
 
   return (
-    <main>
-      <section className='relative max-w-screen-lg px-4 py-10 mx-auto md:flex md:py-10 md:flex-row'>
+    <main className='relative py-6 lg:gap-10 lg:py-10 xl:grid xl:grid-cols-[1fr]'>
+      <section className='mx-auto w-full min-w-0'>
         <div className='flex flex-col items-center justify-center gap-4 w-full'>
-          <header className='flex flex-col gap-4 px-2 py-2 w-full'>
+          <header className='flex flex-col gap-4 py-2 w-full'>
             <div className='flex flex-col gap-4 w-full'>
-              <h2 className='text-6xl text-[#EE81C3] font-bold uppercase'>{title}</h2>
+              <h2 className='text-5xl text-[#EE81C3] font-bold pb-6 border-b border-b-gray-800 uppercase'>{title}</h2>
               <p className='text-gray-50 text-xl'>{description}</p>
             </div>
             <div className='flex items-center justify-between w-full mt-2'>
@@ -40,17 +44,29 @@ export default async function DetailUtility ({ params }: { params: { idUtility: 
             </div>
           </header>
           <div className='relative py-2 w-full'>
-            <span className='absolute top-1 right-4 rounded-full px-2 py-1 bg-[#EE81C3] text-gray-50 font-medium'>HTML</span>
+            <span className='absolute -top-2 right-4 rounded-full text-gray-50 font-medium'>
+              <HtmlIcon width={60} height={60} />
+            </span>
             <Highligter codeString={codeHtml ?? ''} language='html' />
             {codeHtml && <ButtonCopyClipboard code={codeHtml} />}
           </div>
           <div className='relative py-2 w-full'>
-            <span className='absolute top-1 right-4 rounded-full px-2 py-1 bg-[#EE81C3] text-gray-50 font-medium'>CSS</span>
+            <span className='absolute -top-2 right-4 rounded-full text-gray-50 font-medium'>
+              <CssIcon width={60} height={60} />
+            </span>
             <Highligter codeString={codeCss ?? ''} language='css' />
             {codeCss && <ButtonCopyClipboard code={codeCss} />}
           </div>
           <div className='relative py-2 w-full'>
-            <span className='absolute top-1 right-4 rounded-full px-2 py-1 bg-[#EE81C3] text-gray-50 font-medium'>{codeJs ? 'JavaScript' : 'TypeScript'}</span>
+            <span className='absolute top-0 right-7 rounded-full pl-2 text-gray-50 font-medium'>
+              {codeJs
+                ? (
+                  <JsIcon language='javascript' />
+                  )
+                : (
+                  <TsIcon language='typescript' />
+                  )}
+            </span>
             <Highligter codeString={codeJs || codeTs} language={codeJs ? 'javascript' : 'typescript'} />
             {codeJs && <ButtonCopyClipboard code={codeJs} />}
           </div>
